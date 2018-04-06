6f6cfe255754e9469872f0b1d992eb5a7c98e119
Tomas <Tomas@DESKTOP-79I5U28.local>
2018-04-06 15:10:46 +0200

    Přidána možnost spouštět jen tetxtové rozhraní pomocí parametru dle zadání

--- LifeIsDark/src/main/java/com/github/Trinnitti/LifeIsDark/main/Gui.java
+++ LifeIsDark/src/main/java/com/github/Trinnitti/LifeIsDark/main/Gui.java
@@ -3,6 +3,10 @@
  */
 package com.github.Trinnitti.LifeIsDark.main;
 
+import com.github.Trinnitti.LifeIsDark.logika.Hra;
+import com.github.Trinnitti.LifeIsDark.logika.IHra;
+import com.github.Trinnitti.LifeIsDark.ui.TextoveRozhrani;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -26,7 +30,7 @@ public class Gui extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
     	FXMLLoader loader = new FXMLLoader();
-    	loader.setLocation(getClass().getResource("MainWindow.fxml"));    	
+    	loader.setLocation(getClass().getResource("../ui/MainWindow.fxml"));    	
     	Parent root = loader.load();
         
         primaryStage.setScene(new Scene(root));
@@ -35,12 +39,32 @@ public class Gui extends Application {
     }
 
     /***************************************************************************
-     * Metoda, prostřednictvím níž se spouští celá aplikace.
+     * Metoda, prostřednictvím níž se spouští celá aplikace. Při zadání parametru "-text"
+     * se aplikace spustí  textovém rozhraní bez GUI.
      *
      * @param args Parametry příkazového řádku
      */
-    public static void main(String[] args) {
-        launch(args);
+    public static void main(String[] args)
+    {
+
+    	if (args.length == 0) {
+            launch(args);
+        } else {
+            if (args[0].equals("-text")) {
+                IHra hra = new Hra();
+                TextoveRozhrani ui = new TextoveRozhrani(hra);
+                ui.hraj();
+            } else {
+                System.out.println("Neplatný parametr");
+            }
+        }
+    	
+    	//		  TODO parametrické spuštění hry
+//        IHra hra = new Hra();
+//        TextoveRozhrani ui = new TextoveRozhrani(hra);
+//        ui.hraj();
+    	
+    	launch(args);
     }
     
 }
